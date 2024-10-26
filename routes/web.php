3b7714626b5a331883ca92125047f8803c97b2ce
Aastha <pdlpangali111@gmail.com>
2024-10-26 18:11:59 +0545

    doctor appointment list

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
  Route::get('/doctor/dashboard',[DoctorController::class,'docdas'])->name('docdas')->middleware('auth','web','doctor');
- Route::get('/admin/dashboard',[AdminController::class,'admindas'])->name('admindas')->middleware('auth','web','doctor');
- Route::get('/patient/dashboard',[PatientController::class,'patdas'])->name('patdas')->middleware('auth','web','doctor');
+ Route::get('/admin/dashboard',[AdminController::class,'admindas'])->name('admindas')->middleware('auth','web','admin');
+ Route::get('/patient/dashboard',[PatientController::class,'patdas'])->name('patdas')->middleware('auth','web','patient');
 require __DIR__.'/auth.php';
